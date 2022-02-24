69651a0931a9cde8206a9953692926cfad10c376
sergio Diaz <cheyoivan@gmail.com>
2022-02-24 15:40:13 -0500

    primer commit feature

--- src/services/ophthalmologist.service.ts
+++ src/services/ophthalmologist.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 import { Ophthalmologist } from "src/models/ophthalmologist.interface";
 import { User } from "src/models/user.interface";
-import { HttpGenericService } from "./http-generic.service";
+import { HttpGenericService } from "./base/http-generic.service";
+
 
 @Injectable({
     providedIn:'root'
 })
-export class OphthalmologistService extends HttpGenericService<Ophthalmologist>{}
+export class OphthalmologistService {
+    private _ophthalmologist= new Subject<Ophthalmologist>();
+    private _errors= new EventEmitter<String>();
+    constructor(
+        private http: HttpGenericService<Ophthalmologist>
+    ){}
+
+    getE
+}
